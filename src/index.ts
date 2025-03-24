402df0f1f192ca5fa4f98d00c99d14fdb270cd6d
Robin Ong <robinong@zeealdnet.nl>
2025-03-24 07:36:38 +0100

    again

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,8 @@ const cosmosClient = new CosmosClient({
   key: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 });
 
-const container = cosmosClient.database("rental_properties").container("users");
+//const container = cosmosClient.database("rental_properties").container("users");
+const container = cosmosClient.database("rental_properties").container("properties");
 
 // Tool definitions
 const UPDATE_ITEM_TOOL: Tool = {
